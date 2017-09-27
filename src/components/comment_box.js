7f991dc4a61b4b74bd3ba6b15f391a4df775656f
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-09-27 18:02:15 +0200

    form submit event

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 export default class CommentBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { comment: ''};
+    }
+    
+    handleChange(e){
+        this.setState({ comment: e.target.value });
+    }
+
     render() {
         return (
             <div className="comment-box">
-                <textarea/>
+                <textarea 
+                    value={this.state.comment}
+                    onChange={this.handleChange.bind(this)} />
                 <button>YEAH!</button>
             </div>
         )
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -13,5 +13,20 @@ describe('CommentBox', () =>{
     it('has a button', () => {
         expect(component.find('button')).to.exist;        
     });
+
+    describe('entering some text', () => {
+        beforeEach(()=>{
+            component.find('textarea').simulate('change','new comment');
+        });
+
+        it('shows that text in the textarea', () =>{
+            expect(component.find('textarea')).to.have.value('new comment');
+        });
+            
+        it('clear the input after submitting', () => {
+            
+        });
+    });
+
 });
 
