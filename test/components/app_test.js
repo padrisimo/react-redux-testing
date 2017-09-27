af1e6111f256576b0facd53971c30fa7e9e74e27
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-09-27 17:07:52 +0200

    expecting child elements

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,17 +1,10 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
-//decribe - groups similar test 
 describe('App', () => {
-
-  // it - test a single attribute of a target
-  it('shows the correct text', () => {
-
-    //create an instance of App
-    const component = renderComponent(App);
-
-    //expect - assert about a target 
-    expect(component).to.contain('React simple starter');
-
+  const component = renderComponent(App);
+  
+  it('show a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 });
